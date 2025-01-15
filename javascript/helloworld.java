13c478d2b767cc5e7b7137a2eaf30358f28c1a62
Michael-Lee Leduna <leduna59@gmail.com>
2025-01-14 20:46:14 -1000

    Add sd2

--- javascript/helloworld.java
+++ javascript/helloworld.java
@@ -1,7 +1,5 @@
-package javascript;
-
-class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, World!");
     }
 }
