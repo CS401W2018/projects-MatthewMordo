96a529d641fab9251acbcea115699da5f430d73a
Michael-Lee Leduna <leduna59@gmail.com>
2025-01-14 22:39:16 -1000

    add poemss

--- javascript/HaikuPoem.java
+++ javascript/HaikuPoem.java
@@ -2,18 +2,20 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 
-public class HaikuPoem extends JPanel{
+public class HaikuPoem extends JPanel {
     
     @Override
     public void paintComponent(Graphics g){
-
-        //Instead of a joke, I made a haiku about chasing a dangerous love
+        //instead of a joke, I made a poem about chasing a dangerous love
         g.drawString("Flames of Desire", 10, 10);
-        g.drawString("Wild heart in pursuit,", 10, 35);
-        g.drawString("chasing flames that scorch the soul,", 10, 45);
+        g.drawString("------------------------", 10, 15);
+        g.drawString("Wild heart in pursuit,", 10, 25);
+        g.drawString("chasing flames that scorch the soul,", 10, 40);
         g.drawString("love's edge cuts too deep.", 10, 55);
         
     }
+
+    
 
     public static void main(String[] args) {
         System.out.println("HelloCS202!");
@@ -27,7 +29,7 @@ public class HaikuPoem extends JPanel{
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //what to put inside the window
-        window.setContentPane(new GraphicsFun());
+        window.setContentPane(new HaikuPoem());
         
         //make it visible
         window.setVisible(true);
