2d0fa4ea759211a242ceeba2eb758a3aa803d97b
Michael <leduna59@gmail.com>
2025-01-21 12:33:55 -1000

    aaddd asdds

---
+++ javascript/Jan21/MathTextFun.java
@@ -0,0 +1,26 @@
+package Jan21;
+
+public class MathTextFun {
+    
+    public static void main(String[] args) {
+        System.out.println("Plus Operator!");
+        final int RESULT = 5 + 8;
+        System.out.println("Result is: " + (RESULT + 10));
+
+        System.out.println("Subtraction Operator!");
+        int result2 = 5 - 8;
+        System.out.println("Result is: " + result2);
+        
+        System.out.println("Subtraction Operator!");
+        int result3 = 5 - 8;
+        System.out.println("Result is: " + result3);
+
+        System.out.println("Subtraction Operator!");
+        int result4 = 5 - 8;
+        System.out.println("Result is: " + result4);
+
+        System.out.println("Subtraction Operator!");
+        int result5 = 5 - 8;
+        System.out.println("Result is: " + result5);
+    }
+}
