410d8d96d9ec8d0d745bbccfe0ca96fc6fb93653
Michael <leduna59@gmail.com>
2025-04-01 12:53:37 -1000

    add edits

--- javascript/March27/Season.java
+++
@@ -1,7 +0,0 @@
-public class Season {
-
-    public static final String WINTER = "Winter";
-    public static final String SUMMER = "Summer";
-    public static final String FALL = "Fall";
-    public static final String SPRING = "Spring";
-}
